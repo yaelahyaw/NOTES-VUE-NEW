a2fc444326df111c980806d90492945c3d5869b1
MuhamadRyanHidayat <ryanhidayat074@gmail.com>
2020-02-15 11:13:56 +0700

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+    publicPath: process.env.NODE_ENV === 'production' ? '' : './',
     outputDir: './dist',
     indexPath: './index.html',
     devServer: {
